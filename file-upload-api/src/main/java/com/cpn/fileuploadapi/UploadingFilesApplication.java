a09df0fcb3903e79afa0a114256c3895cef5264c
Phanlop Pengsupan <a.pengsupan@gmail.com>
2023-10-23 13:26:44 +0700

    init project

---
+++ file-upload-api/src/main/java/com/cpn/fileuploadapi/UploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.cpn.fileuploadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+
+}
